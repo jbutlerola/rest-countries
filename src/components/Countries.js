1ff61056608491f3d98e1fd15348fa323c72a283
Jack Butler <jackbutlerola@gmail.com>
2022-03-29 21:54:28 -0700

    Added styling and additional functionality

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const Countries = () => {
   const [countries, setCountries] = useState([])
@@ -11,6 +12,7 @@ const Countries = () => {
 
     setCountries(data)
     console.log(data)
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -19,17 +21,21 @@ const Countries = () => {
 
   return (
     <>
-      <section className='grid grid-cols-1 gap-5 p-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 2xl:container 2xl:mx-auto'>
-        {countries.map(({name, region, capital }) => (
-          <article key={name.common} className="bg-white rounded shadow p-5">
-            <h2 className='font-bold text-grey-900 text-2xl mb-3'>{name.common}</h2>
-            <ul>
-              <li>Capital: {capital}</li>
-              <li>Region: {region}</li>
-            </ul>
-          </article>
-        ))}
-      </section>
+      {isLoading ? <h1 className='flex items-center justify-center h-screen text-4xl uppercase tracking-widest text-gray-900 dark:text-white lg:text-7xl font-bold'>Loading...</h1> : 
+        <section className='grid grid-cols-1 gap-5 p-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 2xl:container 2xl:mx-auto pt-28'>
+          {countries.map(({name, region, capital }) => (
+            <Link to={`/${name.common}`} key={name.common}> 
+              <article className="bg-white rounded shadow p-5 dark:bg-gray-800 dark:text-white hover:dark:bg-gray-700 hover:bg-gray-100 transition-all duration-300">
+                <h2 className='font-bold text-grey-900 text-2xl mb-3 '>{name.common}</h2>
+                <ul>
+                  <li>Capital: {capital}</li>
+                  <li>Region: {region}</li>
+                </ul>
+              </article>
+            </Link>
+          ))}
+        </section>
+      }
     </>
   )
 }
