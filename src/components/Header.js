9ec3adabafbe6e9fcb75ab4fb4333f9622f11dcb
Jack Butler <jackbutlerola@gmail.com>
2022-03-29 21:56:17 -0700

    added header styling and functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Toggle from './Toggle'
 
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <header className="flex items-center justify-between fixed w-full bg-white shadow p-5 dark:bg-gray-800">
+        <div>
+          <h1 className='font-bold text-gray-900 dark:text-white text-2xl'>Where in the world?</h1>
+        </div>
+
+        <div>
+          <Toggle />
+        </div>
+      </header>
     </div>
   )
 }
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
+import {FaMoon, FaSun} from "react-icons/fa"
 
 const Toggle = () => {
+const [darkMode, setDarkMode] = useState(true)
+
+const handleClick = () => {
+  document.body.classList.toggle("dark")
+  setDarkMode(!darkMode)
+}
+
   return (
-    <div>
-      <button>Toggle</button>
-    </div>
+    <>
+      <button onClick={handleClick}>
+        {darkMode ? <FaMoon className='text-gray-900' /> : <FaSun className='text-yellow-500' /> }
+      </button>
+    </>
   )
 }
 
