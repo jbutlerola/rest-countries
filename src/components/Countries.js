cf5918ccfd034cb97b48aa22e96e452a7063c993
Jack Butler <jackbutlerola@gmail.com>
2022-04-12 21:31:15 -0700

    started adding filter by region

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -40,7 +40,11 @@ const Countries = () => {
       {isLoading ? <h1 className='flex items-center justify-center h-screen text-4xl uppercase tracking-widest text-gray-900 dark:text-white lg:text-7xl font-bold'>Loading...</h1> : 
         <>
           <div className='pt-32'>
-            <Filter searchCountries={searchCountries} searchInput={searchInput} />
+            <Filter 
+            searchCountries={searchCountries} 
+            searchInput={searchInput}
+            setCountries={setCountries}
+             />
           </div>
           {searchInput.length > 0 ? 
             <section className='grid grid-cols-1 gap-5 p-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 2xl:container 2xl:mx-auto'>
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,39 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
-const Filter = ({searchCountries, searchInput}) => {
+const Filter = ({searchCountries, searchInput, setCountries}) => {
+
+  const region = [
+    {
+      name: "Africa",
+    },
+    {
+      name: "America",
+    },
+    {
+      name: "Asia",
+    },
+    {
+      name: "Europe",
+    },
+    {
+      name: "Oceania",
+    },
+  ]
+
+  const fetchCountryByRegion = async (region) => {
+    const res = await fetch(`https://restcountries.com/v3.1/region/${region}`)
+    const data = await res.json()
+    // TODO: figure out why region will not be put on url 
+    // setCountries(data)
+  }
+
+
+  useEffect(() => {
+    fetchCountryByRegion()
+  }, [])
+
+
   return (
     <>
       <div className='flex items-start justify-between flex-col md:flex-row md:items-center md:justify-between 2xl:container 2xl:mx-auto'>
@@ -11,7 +44,12 @@ const Filter = ({searchCountries, searchInput}) => {
         className='py-2 px-4 rounded shadow placeholder-gray-900 ml-5 lg:w-1/2 sm:w-full dark:placeholder-white dark:'
         autoComplete='off'
          />
-         <select className='ml-5' name="select" id="select">
+         <select className=' py-2 px-4 rounded shadow ml-5 md:mr-5 md:ml-0'
+          name="select" 
+          id="select"
+          value={region.name}
+          onChange={(e) => fetchCountryByRegion(e.target.value)}
+          >
            <option value="Africa">Africa</option>
            <option value="Americas">Americas</option>
            <option value="Asia">Asia</option>
